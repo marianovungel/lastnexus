3cad3041466bb69a154c3ff7078d734f952b477c
marianovungel <vungem88@gmail.com>
2024-10-24 15:01:45 -0300

    Link

--- components/ArtigoList/ArtigoList.jsx
+++ components/ArtigoList/ArtigoList.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import { IoLockClosed } from "react-icons/io5";
 import { CiMenuKebab } from "react-icons/ci";
 import { IoDocumentTextOutline } from "react-icons/io5";
-import { useRouter } from 'next/router';
+// import { useRouter } from 'next/router';
 
 export default function ArtigoList({ data }) {
-  const router = useRouter()
+  // const router = useRouter()
+  const OpenArgito = (docId)=>{
+    window.location.replace(`https://nexus-editor.vercel.app/documents/${docId}`)
+  }
   return (
     <>
     {data.map((data)=>(
-      <div onClick={()=>{router.push(`/createDocs/${data._id}`)}} key={data?._id} id='artigolist'  className='w-full py-2 flex flex-row justify-between items-end text-#666 cursor-pointer hover:bg-[#F5F5F5]'>
+      <div onClick={()=>OpenArgito(data._id)} key={data?._id} id='artigolist'  className='w-full py-2 flex flex-row justify-between items-end text-#666 cursor-pointer hover:bg-[#F5F5F5]'>
           <div className='flex flex-row justify-start items-center gap-2 pl-2'>
             <IoDocumentTextOutline  className='text-xl text-blue-700' />
             <b className='titleArtigoItemCss text-sm'>{data?.title}</b>
