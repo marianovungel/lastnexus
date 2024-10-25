a42364b7a4f2fa64e8c47dd0c6efb9ddb5522174
marianovungel <vungem88@gmail.com>
2024-10-25 15:46:42 -0300

    new update

--- pages/login/index.js
+++ pages/login/index.js
@@ -129,7 +129,7 @@ export default function Login() {
             <label className='w-full flex items-center justify-between cursor-pointer' htmlFor="file">
               {/* <img className='w-10 h-10 rounded-full object-cover' src={avatar.url || "default-avatar.png"} alt="" /> */}
               <Image 
-                src={avatar.url ? avatar.url : AvatarULR }
+                src={avatar.url !== "" ? avatar.url : AvatarULR }
                 alt="User"
                 width={100}
                 height={100}
@@ -137,17 +137,7 @@ export default function Login() {
                 fetchpriority="high"
                 className='w-10 h-10 rounded-full object-cover'
               />
-              {!avatar.url &&
-                <Image 
-                src={AvatarULR}
-                alt="User"
-                width={100}
-                height={100}
-                priority={true}
-                fetchpriority="high"
-                className='w-10 h-10 rounded-full object-cover'
-              />
-              }
+              
               Adicionar foto do perfil
             </label>
             <input className='p-5 border-none outline-none bg-[rgba(17, 25, 40, 0.6)] text-[#666] rounded-[5px]' type="file" id='file' style={{ display: "none" }} onChange={handleAvatar} />
